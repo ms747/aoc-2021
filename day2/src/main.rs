5a3de3d6dc3c60229fc02d69f3d5522e21921f6a
Your Name <you@example.com>
2021-12-02 18:47:59 +0530

    Micro optimizations

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,8 +1,8 @@
 use std::time::Instant;
 
-fn mapper(course: &[&str]) -> (i32, i32) {
-    let num: i32 = course[1].parse().unwrap();
-    match course[0] {
+fn mapper(course: (&str, &str)) -> (i32, i32) {
+    let num: i32 = course.1.parse().unwrap();
+    match course.0 {
         "forward" => (num, 0),
         "up" => (0, -num),
         "down" => (0, num),
@@ -10,25 +10,26 @@ fn mapper(course: &[&str]) -> (i32, i32) {
     }
 }
 
-fn part1(input: &[Vec<&str>]) -> i32 {
+fn part1(input: &[(&str, &str)]) -> i32 {
     let compute = input
         .iter()
-        .map(|course| mapper(&course[..]))
+        .map(|course| mapper(*course))
         .fold((0, 0), |(hor, dep), (x, y)| (hor + x, dep + y));
 
     compute.0 * compute.1
 }
 
-fn part2(input: &[Vec<&str>]) -> i32 {
-    let compute = input.iter().map(|course| mapper(&course[..])).fold(
-        (0, 0, 0),
-        |(hor, dep, aim), (x, y)| {
-            if y == 0 {
-                return (hor + x, dep + aim * x, aim);
-            }
-            (hor, dep, aim + y)
-        },
-    );
+fn part2(input: &[(&str, &str)]) -> i32 {
+    let compute =
+        input
+            .iter()
+            .map(|course| mapper(*course))
+            .fold((0, 0, 0), |(hor, dep, aim), (x, y)| {
+                if y == 0 {
+                    return (hor + x, dep + aim * x, aim);
+                }
+                (hor, dep, aim + y)
+            });
 
     compute.0 * compute.1
 }
@@ -36,7 +37,7 @@ fn part2(input: &[Vec<&str>]) -> i32 {
 fn main() {
     let input: Vec<_> = include_str!("input.txt")
         .lines()
-        .map(|line| line.split(' ').collect::<Vec<&str>>())
+        .map(|line| line.split_once(' ').unwrap())
         .collect();
 
     let it = Instant::now();
